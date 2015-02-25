4b6b3b26e8d0bf56ac70c4421a16d3561035949c
Stephen Done <srdone@gmail.com>
2015-02-24 19:18:03 -0700

    plays

--- mainController.js
+++ mainController.js
@@ -1,9 +1,15 @@
-angular.module('sounder').controller('MainController', ['$scope', 'soundService', function ($scope, soundService) {
+angular.module('sounder').controller('MainController', ['$scope', '$sce', 'soundService', function ($scope, $sce, soundService) {
 
   $scope.getUser = function () {
     soundService.getUser($scope.searchText).then(function (data) {
-      console.log(data);
+      $scope.userData = data;
     });
   };
+
+  $scope.play = function (track_url) {
+    SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+      $scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
+    });
+  }
 
 }]);
